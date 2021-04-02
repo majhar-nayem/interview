f85c3d29b420e53835fa2c9c52fb6a0f704f298e
majhar-nayem <majharul.islam02@northsouth.edu>
2021-04-02 16:12:43 +0600

    password string changed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,7 +22,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$12$/UOB1i8eWBonRXFudmhmTeyLxBzmJ1hqbEmbHj2vnmUe3ZK1jWMcG ', // pass1234
         'remember_token' => Str::random(10),
     ];
 });
